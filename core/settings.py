d548dbc3eb7b5bcb8ca601a33e2c64f5920fbebd
SoftCysec <softengdedan@gmail.com>
2022-12-09 23:30:26 +0300

    settings modified

--- core/settings.py
+++ core/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0q%_w$%ysprytbnz%%9_(onevk$8-151sa@#)$ri19hk+ie36a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://diseasepredictdjango-production.up.railway.app/']
+ALLOWED_HOSTS = ['127.0.0.1','diseasepredictdjango-production.up.railway.app/']
 
 
 # Application definition
